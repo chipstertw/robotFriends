1f01ed1c68d202d28a960bb6637952bc00514111
Yichi <yichi@YichiMBP.local>
2020-06-13 16:41:35 -0400

    made a build folder

---
+++ build/precache-manifest.ad3ec4a34da640052cc468ee879c5c13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ae00f4cc4bdb5a09e2ab90cd7245dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fda44699e0e441b869ce",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "943f89cc49e6367ec807",
+    "url": "/static/css/main.8dc8d6b4.chunk.css"
+  },
+  {
+    "revision": "fda44699e0e441b869ce",
+    "url": "/static/js/2.cd09866f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd09866f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943f89cc49e6367ec807",
+    "url": "/static/js/main.db82f710.chunk.js"
+  },
+  {
+    "revision": "d09714fffcb35a234207",
+    "url": "/static/js/runtime-main.3c599875.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
